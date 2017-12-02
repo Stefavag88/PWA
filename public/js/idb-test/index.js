ead9ba14e2c2c51e040782f0d34278b708a9c00b
Vaggelis Stefanakis <stefanakisvag@gmail.com>
2017-12-02 17:47:06 +0200

    Indexed DB --Put an array of Objects into a store

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,31 +1,87 @@
 import idb from 'idb';
 
-var dbPromise = idb.open('test-db', 1, function(upgradeDb) {
-  var keyValStore = upgradeDb.createObjectStore('keyval');
-  keyValStore.put("world", "hello");
+const stores = {
+  keyval: 'keyval',
+  people: 'people'
+}
+
+const { keyval, people } = stores;
+
+const customers = [
+  {
+    id: 1,
+    name: 'Tzolas Mitsos',
+    age: 30
+  },
+  {
+    id: 2,
+    name: 'Vag Stef',
+    age: 29
+  },
+  {
+    id: 3,
+    name: 'Antoniadis Konstantinos',
+    age: 28
+  },
+  {
+    id: 4,
+    name: 'Mr. George',
+    age: 54
+  },
+];
+
+
+const dbPromise = idb.open('test-db', 2, upgradeDb => {
+
+  switch (upgradeDb.oldVersion) {
+    case 0:
+      const keyValStore = upgradeDb.createObjectStore(keyval);
+      keyValStore.put("world", "hello");
+    //do not use break; here -- we want the code to continue so that every old version is checked!
+    case 1:
+      upgradeDb.createObjectStore(people, { keyPath:'id' });
+  }
 });
 
 // read "hello" in "keyval"
-dbPromise.then(function(db) {
-  var tx = db.transaction('keyval');
-  var keyValStore = tx.objectStore('keyval');
+dbPromise.then(db => {
+  const tx = db.transaction(keyval);
+  const keyValStore = tx.objectStore(keyval);
   return keyValStore.get('hello');
-}).then(function(val) {
+}).then(function (val) {
   console.log('The value of "hello" is:', val);
 });
 
 // set "foo" to be "bar" in "keyval"
-dbPromise.then(function(db) {
-  var tx = db.transaction('keyval', 'readwrite');
-  var keyValStore = tx.objectStore('keyval');
+dbPromise.then(db => {
+  const tx = db.transaction(keyval, 'readwrite');
+  const keyValStore = tx.objectStore(keyval);
   keyValStore.put('bar', 'foo');
   return tx.complete;
-}).then(function() {
+}).then(function () {
   console.log('Added foo:bar to keyval');
 });
 
-dbPromise.then(function(db) {
-  // TODO: in the keyval store, set
-  // "favoriteAnimal" to your favourite animal
-  // eg "cat" or "dog"
-});
+//set favouriteAnimal to cat
+dbPromise.then(db => {
+  const tx = db.transaction(keyval, 'readwrite');
+  const keyValStore = tx.objectStore(keyval);
+  keyValStore.put('cat', 'favoriteAnimal');
+  return tx.complete;
+}).then(() => {
+  console.log('Added favoriteAnimal : cat');
+})
+
+dbPromise.then(db => {
+
+  const tx = db.transaction(people, 'readwrite');
+  const peopleStore = tx.objectStore(people);
+
+  customers.forEach( cust => {
+    peopleStore.put(cust);
+  });
+ 
+  return tx.complete;
+}).then(() => {
+  console.log('Added customers', customers);
+})
